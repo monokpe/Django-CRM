6d6c81381b599da7c08263a958bcca3af8f458e7
James Onokpe <monokpe@gmail.com>
2024-06-16 20:09:58 -0600

    Registration form

--- website/views.py
+++ website/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def home(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, "You have been logged in!".title())
+            return redirect("home")
+        messages.error(
+            request, "There was an error logging in, please try again...".title()
+        )
+        return redirect("home")
+    return render(request, "home.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out...".title())
+    return redirect("home")
+
+
+def register_user(request):
+    return render(request, "register.html", {})
